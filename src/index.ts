71aabb8130084fa8e9930e042dfe2c209cdd86e5
Guilherme Santos <guilhermejeffofc@gmail.com>
2024-05-24 19:31:36 -0300

    upload source 0.1

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Express, Request, Response } from 'express';
+import { PORT } from './secrets';
+
+const app: Express = express();
+
+app.get('/', (req:Request, res:Response) => {
+	res.send("Working");
+})
+
+app.listen(PORT, () => {console.log('App Working!')})
